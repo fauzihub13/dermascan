822ebc4bd51fb1d7e73db1d54c6b8622c0522789
fauzihub13 <fauziadisaputra3@gmail.com>
2025-04-27 21:22:19 +0700

    feat: change base model to 77% test accuracy

--- lib/src/core/utils/constant.dart
+++ lib/src/core/utils/constant.dart
@@ -1,6 +1,6 @@
 class Constant {
   // static const modelPath = 'assets/models/model_mobilenetv2_43p.tflite';
   // static const labelsPath = 'assets/models/labels.txt';
-  static const modelPath = 'assets/models/skin_disease_10classes_densenet121.tflite';
+  static const modelPath = 'assets/models/skin_disease_10classes_densenet121_agil.tflite';
   static const labelsPath = 'assets/models/label_10classes.txt';
 }
