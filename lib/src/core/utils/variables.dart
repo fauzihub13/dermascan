26cc7db94a8501012ca5bf8e5c4e49df4b60ae70
fauzihub13 <fauziadisaputra3@gmail.com>
2025-04-29 16:59:44 +0700

    fix: change base api url

--- lib/src/core/utils/variables.dart
+++ lib/src/core/utils/variables.dart
@@ -1,4 +1,4 @@
 class Variables {
-  static const String baseApiUrl = 'http://dermascan.sunify.my.id/api/v1';
-  static const String baseUrl = 'http://dermascan.sunify.my.id';
+  static const String baseApiUrl = 'http://dermascan.codira.my.id/api/v1';
+  static const String baseUrl = 'http://dermascan.codira.my.id';
 }
